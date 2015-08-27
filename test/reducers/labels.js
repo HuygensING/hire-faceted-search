9c601e4dcc7824660e1c67090fa88b663eff8387
Rene van der Ark <r.van.der.ark@gmail.com>
2015-08-27 12:25:18 +0200

    added first test

---
+++ test/reducers/labels.js
@@ -0,0 +1,32 @@
+import expect from "expect";
+import reducer from "../../src/reducers/labels";
+
+
+
+describe('labels reducer', () => {
+	it('should have the correct initial state', () => {
+		expect(
+			reducer(undefined, {})
+		).toEqual({
+			facetTitles: {},
+			newSearch: "New search",
+			resultsFound: "results",
+			showAll: "Show all",
+			sortBy: "Sort by"
+		});
+	});
+
+
+	it('should handle SET_LABELS', () => {
+		expect(
+			reducer({initial: "state"}, {
+				type: "SET_LABELS",
+				labels: {foo: "bar", baz: "bax"}
+			})
+		).toEqual({
+			initial: "state",
+			foo: "bar",
+			baz: "bax"
+		});
+	});
+});
