f7e7d74ae0947828fe0c3a6c03b248a6f3cc00dd
Rene van der Ark <r.van.der.ark@gmail.com>
2015-08-27 14:24:23 +0200

    added pending tests for queries, coffee break

---
+++ test/reducers/queries.js
@@ -0,0 +1,12 @@
+import expect from "expect";
+import reducer from "../../src/reducers/queries";
+
+describe('queries reducer', () => {
+	it("should handle SET_QUERY_DEFAULTS");
+	it("should handle SET_RESULTS_SORT");
+	it("should handle INIT_SORT_PARAMS");
+	it("should handle REMOVE_FACET_VALUE");
+	it("should handle ADD_FACET_VALUE");
+	it("should handle CHANGE_SEARCH_TERM");
+	it("should handle NEW_SEARCH");
+});
