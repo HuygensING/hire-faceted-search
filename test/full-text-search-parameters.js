a1e9b877f18c5ae6b9e9974ebc68c26fc9da7109
renevanderark <r.van.der.ark@gmail.com>
2015-11-06 13:12:40 +0100

    finished full-text-search-parameters test

--- test/full-text-search-parameters.js
+++ test/full-text-search-parameters.js
@@ -1,5 +1,6 @@
 import React from "react/addons";
 import sinon from "sinon";
+import expect from "expect";
 import FacetedSearch from "../src";
 import {server} from "../src/actions/results";
 
@@ -9,23 +10,34 @@ import sd from "skin-deep";
 describe("FacetedSearch", function() {
 	it("should update the fullTextSearchParameters on receiving next fullTextSearchParameters prop", () => {
 		var props = { config: {} };
-		let tree = sd.shallowRender(<FacetedSearch {...props}/>);
 
 		// Stub performXhr function of server
 		sinon.stub(server, "performXhr", (options, cb) => {
 			// perform assertions on options here,
 			// optionally invoke cb and do assertion on its results (should be in scope of this test)
-			console.log(options, cb);
+			expect(JSON.parse(options.data)).toEqual({
+				facetValues: [],
+				sortParameters: [],
+				term: "",
+				fullTextSearchParameters: [{name: "foo", term: "bar"}]
+			});
+			expect(typeof cb).toEqual("function");
 		});
+
+		let tree = sd.shallowRender(<FacetedSearch {...props}/>);
+
+		// shallowRender does not trigger componentDidMount so initial search is not called
 
 
 		props.query = {
 			fullTextSearchParameters: [{name: "foo", term: "bar"}]
 		};
 
+		// Render should trigger willComponentUpdate, invoke setQuery and dispatch the new request here
 		tree.reRender(<FacetedSearch {...props}/>);
 
-		let fs = tree.getMountedInstance();
+		// assert that the stub was invoked
+		sinon.assert.calledOnce(server.performXhr);
 
 		// always restore a stubbed function at the end of the test
 		// (or else it will still be stubbed in a next test)
